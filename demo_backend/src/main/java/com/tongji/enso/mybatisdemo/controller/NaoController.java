c5679b76429394b7a2aa114aac750d442bc2ce4e
lei <xiang.lei.se@foxmail.com>
2023-08-01 14:31:51 +0800

    改成echarts格式，TODO: 第一张图的经纬度数据

--- demo_backend/src/main/java/com/tongji/enso/mybatisdemo/controller/NaoController.java
+++ demo_backend/src/main/java/com/tongji/enso/mybatisdemo/controller/NaoController.java
@@ -2,16 +2,13 @@ package com.tongji.enso.mybatisdemo.controller;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
-import com.tongji.enso.mybatisdemo.mapper.online.EnsoMapper;
 import com.tongji.enso.mybatisdemo.mapper.online.NaoMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+
 
 @RestController
 @RequestMapping("/nao")
@@ -19,6 +16,35 @@ public class NaoController {
 
     @Autowired
     private NaoMapper naoMapper;
+
+    private final InfoController infoController;
+
+    public NaoController(InfoController infoController) {
+        this.infoController = infoController;
+    }
+
+
+    /**
+     * 创建系列数据对象
+     * @param name
+     * @param type
+     * @param lineType
+     * @param data
+     * @return
+     */
+    private <T> Map<String, Object> createSeries(String name, String type, String lineType, List<T> data) {
+        Map<String, Object> series = new HashMap<>();
+        series.put("name", name);
+        series.put("type", type);
+        if (lineType != null) {
+            Map<String, Object> lineStyle = new HashMap<>();
+            lineStyle.put("type", lineType);
+            series.put("lineStyle", lineStyle);
+        }
+        series.put("data", data);
+        return series;
+    }
+
 
     /**
      * 根据年月，返回该时起报，模型预报6个月的误差 （for 预测结果误差分布图）
@@ -62,8 +88,41 @@ public class NaoController {
             errorDataList3D.add(errorDataList2D);
         }
 
-        Map<String, Object> result = new HashMap<>();
-        result.put("data", errorDataList3D);
+        // 获取经纬度数据
+        Map<String, List<Double>> latLonData = infoController.getAllLatLon();
+        List<Double> latList = latLonData.get("lat");
+        List<Double> lonList = latLonData.get("lon");
+
+        Map<String, Object> result = new HashMap<>();  // 存放总的六个 option
+
+        for (int i = 0; i < 6; i++)
+        {
+            Map<String, Object> eachResult = new HashMap<>();  // 存放当 option，利用 for 循环更改其中的数据
+
+            Map<String, Object> title = new HashMap<>();
+            title.put("text", "提前" + (i + 1) + "个月预测");
+            title.put("left", "center");
+            eachResult.put("title", title);
+
+            Map<String, Object> tooltip = new HashMap<>();
+            eachResult.put("tooltip", tooltip);
+
+            Map<String, Object> xAxis = new HashMap<>();
+            xAxis.put("type", "category");
+            xAxis.put("name", "经度");
+//            xAxis.put("data", lonList);
+            eachResult.put("xAxis", xAxis);
+
+            Map<String, Object> yAxis = new HashMap<>();
+            yAxis.put("type", "category");
+            yAxis.put("name", "纬度");
+//            yAxis.put("data", latList);
+            eachResult.put("yAxis", yAxis);
+
+            eachResult.put("series", createSeries("ours", "heatmap", null, errorDataList3D.get(i)));
+
+            result.put("option" + (i + 1), eachResult);
+        }
 
         return result;
     }
@@ -80,19 +139,55 @@ public class NaoController {
         String allCorrNAOMCDData = naoMapper.findTJNaoResultByMonthType("all", "all", "corr_lead1-6_NAO-MCD");
 
         Gson gson = new Gson();
-        List<Double> allCorrECMWFList1D = gson.fromJson(allCorrECMWFData, new TypeToken<List<Double>>() {
-        }.getType());
-        List<Double> allCorrECCCList1D = gson.fromJson(allCorrECCCData, new TypeToken<List<Double>>() {
-        }.getType());
-        List<Double> allCorrNAOMCDList1D = gson.fromJson(allCorrNAOMCDData, new TypeToken<List<Double>>() {
-        }.getType());
+        List<Double> allCorrECMWFList1D = gson.fromJson(allCorrECMWFData, new TypeToken<List<Double>>() {}.getType());
+        List<Double> allCorrECCCList1D = gson.fromJson(allCorrECCCData, new TypeToken<List<Double>>() {}.getType());
+        List<Double> allCorrNAOMCDList1D = gson.fromJson(allCorrNAOMCDData, new TypeToken<List<Double>>() {}.getType());
 
         Map<String, Object> result = new HashMap<>();
-        result.put("corrECMWFValues", allCorrECMWFList1D);
-        result.put("corrECCCValues", allCorrECCCList1D);
-        result.put("corrNAOMCDValues", allCorrNAOMCDList1D);
+
+
+        Map<String, Object> title = new HashMap<>();
+        title.put("text", "月度NAOI预测技巧");
+        title.put("left", "center");
+        result.put("title", title);
+
+        Map<String, Object> tooltip = new HashMap<>();
+        result.put("tooltip", tooltip);
+
+        Map<String, Object> xAxis = new HashMap<>();
+        xAxis.put("type", "category");
+        xAxis.put("name", "预测提前期（月）");
+        Map<String, Object> axisLine = new HashMap<>();
+        Map<String, Object> lineStyle = new HashMap<>();
+        lineStyle.put("color", "black");
+        axisLine.put("lineStyle", lineStyle);
+        axisLine.put("onZero", false);
+        xAxis.put("axisLine", axisLine);
+        xAxis.put("data", Arrays.asList("1", "2", "3", "4", "5", "6"));
+        result.put("xAxis", xAxis);
+
+        Map<String, Object> yAxis = new HashMap<>();
+        yAxis.put("type", "value");
+        yAxis.put("name", "相关系数技巧");
+        yAxis.put("min", -0.2);
+        yAxis.put("data", Arrays.asList(-0.2, 0.0, 0.2, 0.4, 0.6, 0.8));
+        result.put("yAxis", yAxis);
+
+        Map<String, Object> legend = new HashMap<>();
+        legend.put("data", Arrays.asList("95% significance", "ECMWF", "ECCC", "NAO-MCR"));
+        legend.put("orient", "horizontal");
+        legend.put("left", "center");
+        legend.put("bottom", "5");
+        result.put("legend", legend);
+
+        List<Map<String, Object>> series = Arrays.asList(
+                createSeries("95% significance", "line", "dashed", Arrays.asList(0.35, 0.35, 0.35, 0.35, 0.35, 0.35)),
+                createSeries("ECMWF", "line", null, allCorrECMWFList1D),
+                createSeries("ECCC", "line", null, allCorrECCCList1D),
+                createSeries("NAO-MCR", "line", null, allCorrNAOMCDList1D)
+        );
+        result.put("series", series);
 
         return result;
     }
-
 }
